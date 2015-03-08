c63af0f7379d59667f35034e6a16a7b18833ecf0
Jonathan <jhow.tan@gmail.com>
2015-03-08 16:57:55 +0800

    Update jeromelebel-mongohub.rb
    Update download and appcast url

--- Casks/jeromelebel-mongohub.rb
+++ Casks/jeromelebel-mongohub.rb
@@ -2,9 +2,10 @@ cask :v1 => 'jeromelebel-mongohub' do
   version :latest
   sha256 :no_check
 
-  # amazonaws.com is the official download host per the vendor homepage
-  url 'https://mongohub.s3.amazonaws.com/MongoHub.zip'
-  appcast 'https://mongohub.s3.amazonaws.com/mongohub_su_feed.xml'
+  # amazonaws.com link to MongoHub download is now down
+  # direct download from github host for MongoHub.zip
+  url 'https://github.com/downloads/bububa/MongoHub-Mac/MongoHub.zip'
+  appcast 'http://mongohub.todayclose.com/appcast.xml'
   name 'MongoHub'
   homepage 'https://github.com/jeromelebel/MongoHub-Mac'
   license :oss
